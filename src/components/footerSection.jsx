5ca6957ac5080c1b1e72edf4b6ad77130b2026b3
Sandy <sandy3zero@gmail.com>
2025-02-22 01:08:04 +0530

    added arrowButton and responsive contact button

--- src/components/footerSection.jsx
+++ src/components/footerSection.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import arrowButton from '../assets/arrowButton.svg'
 
 const FooterSection = () => {
     return(
-        <div className="w-full h-100vh bg-black flex flex-col items-center justify-center pb-20">
-      <button className=" font-chokokutai text-[#EFC7B0] border-1 border-[#EFC7B0] px-16 py-3 ">
-        Contact
-      </button>
+      <>
+        <div className="w-full h-100vh bg-black flex flex-col items-center justify-center pb-20 ">
+          <a
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=your-email@gmail.com&su=Contact%20Inquiry&body=Hello,%20I%20would%20like%20to%20connect!"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <button className="font-chokokutai text-xl text-[#EFC7B0] border border-[#EFC7B0] px-10 py-3 flex items-center  hover:bg-[#8f6046] hover:text-white transition duration-700 gap-5">
+              Contact us
+              <img src={arrowButton} alt="arrowButton" className="w-6 h-6" /> 
+            </button>
+         </a>
+
         </div>  
+      </>
         
     );
 };
