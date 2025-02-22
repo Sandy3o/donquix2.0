01b5967206fc786778a2383a8cbdcab5e31700d4
Sandy <sandy3zero@gmail.com>
2025-02-23 02:26:10 +0530

    added side buttons in heroSection

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -5,30 +5,31 @@ import logo from "../assets/logo.svg";
 const Navbar = () => {
   return (
     <nav className="w-full h-[10vh] flex flex-col sm:flex-row items-center justify-between px-6 sm:px-10 py-4 bg-black">
-      {/* Left Section: Logo & Stars */}
-      <div className="flex items-center gap-3">
+      {/* Left Section */}
+      <div className="flex items-center gap-5">
         <img src={logo} alt="Donquix Logo" className="w-8 h-8" />
         <span className="text-[#EFC7B0] text-lg font-chokokutai">Donquix</span>
         <span className="text-[#EFC7B0] hidden sm:inline">|</span>
 
-        {/* Stars (Left) */}
-        <div className="flex gap-1">
+        {/*left star*/}
+        <div className="flex gap-5">
           {[...Array(3)].map((_, i) => (
             <img key={i} src={star} alt="star" className="w-4 h-4" />
           ))}
         </div>
       </div>
 
-      {/* Right Section: Stars & Tagline */}
+      {/* Right Section */}
       <div className="flex flex-col sm:flex-row items-center gap-3 mt-2 sm:mt-0">
-        {/* Stars (Right) */}
-        <div className="flex gap-1">
+        
+        {/*right star) */}
+        <div className="flex gap-5">
           {[...Array(3)].map((_, i) => (
             <img key={i} src={star} alt="star" className="w-4 h-4" />
           ))}
         </div>
         <span className="text-[#EFC7B0] text-center sm:text-right text-sm sm:text-base">
-          | UNCOMMON among UNCOMMON
+          |  UNCOMMON among UNCOMMON
         </span>
       </div>
     </nav>
