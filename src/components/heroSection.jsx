6575798cfad77cab96fb286807e758cb3329ebb8
Sandy <sandy3zero@gmail.com>
2025-07-17 02:36:22 +0530

    add buttonbg

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,15 +1,13 @@
 import heroImage from "../assets/heroImage.svg";
 import ellipse from "../assets/ellipse.svg";
-// import buttonBackground from '../assets/buttonBackground';
+import buttonBackground from "../assets/buttonBackground.svg";
 import "../App.css";
 
 const HeroSection = () => {
   return (
     <>
-      <div style={{ transform: "scaleX(-1)" }}>
-        <RightCurved />
-      </div>
       <RightCurved />
+      <LeftCurved />
 
       <section className="w-ful bg-black flex flex-col items-center justify-items-normal text-center mb-10">
         {/* Hero Image */}
@@ -37,63 +35,82 @@ const HeroSection = () => {
         </div>
 
         {/* Work Button */}
-        <div className="absolute left-55 top-80 space-y-6">
+        <div className="absolute left-57 top-80">
           <a
             href="/work"
-            alt="work"
-            className="relative block px-6 py-3 text-[#EAD0B2] text-2xl font-Gilro italic"
+            className="relative inline-block px-6 py-3 text-[#EAD0B2] text-2xl font-Gilro italic"
           >
-            {/* <span
-            className="absolute inset-0 bg-no-repeat bg-center bg-cover"
-            style={{ buttonBackground }}
-          /> */}
-            <span className="relative">Work</span>
+            {/* SVG Background */}
+            <span className="absolute inset-0 -z-[-1]">
+              <img
+                src={buttonBackground}
+                alt="ButtonBackground"
+                className="w-full h-full object-fill"
+              />
+            </span>
+
+            {/* Button Text */}
+            <span className="relative z-10">Work</span>
           </a>
         </div>
 
         {/*Service Button */}
 
-        <div className="absolute left-50 top-130 space-y-6">
+        <div className="absolute left-55 top-130">
           <a
-            href="/Service"
-            alt="Service"
-            className="relative block px-6 py-3 text-[#EAD0B2] text-2xl font-Gilro italic"
+            href="/work"
+            className="relative inline-block px-6 py-3 text-[#EAD0B2] text-2xl font-Gilro italic"
           >
-            {/* <span
-            className="absolute inset-0 bg-no-repeat bg-center bg-cover"
-            style={{ buttonBackground }}
-          /> */}
-            <span className="relative">Service</span>
+            {/* SVG Background */}
+            <span className="absolute inset-0 -z-[-1]">
+              <img
+                src={buttonBackground}
+                alt="ButtonBackground"
+                className="w-full h-full object-fill"
+              />
+            </span>
+
+            {/* Button Text */}
+            <span className="relative z-10">Service</span>
           </a>
         </div>
-
         {/* Blogs Button */}
-        <div className="absolute right-55 top-80 space-y-6">
+        <div className="absolute right-55 top-80">
           <a
-            href="/Blogs"
-            alt="Blogs"
-            className="relative block px-6 py-3 text-[#EAD0B2] text-2xl font-Gilro italic"
+            href="/work"
+            className="relative inline-block px-6 py-3 text-[#EAD0B2] text-2xl font-Gilro italic"
           >
-            {/* <span
-            className="absolute inset-0 bg-no-repeat bg-center bg-cover"
-            style={{ buttonBackground }}
-          /> */}
-            <span className="relative">Blogs</span>
+            {/* SVG Background */}
+            <span className="absolute inset-0 -z-[-1]">
+              <img
+                src={buttonBackground}
+                alt="ButtonBackground"
+                className="w-full h-full object-fill"
+              />
+            </span>
+
+            {/* Button Text */}
+            <span className="relative z-10">Blogs</span>
           </a>
         </div>
 
         {/*HandBook button  */}
-        <div className="absolute right-50 top-130 space-y-6">
+        <div className="absolute right-49 top-130">
           <a
-            href="/HandBook"
-            alt="HandBook"
-            className="relative block px-6 py-3 text-[#EAD0B2] text-2xl font-Gilro italic"
+            href="/work"
+            className="relative inline-block px-6 py-3 text-[#EAD0B2] text-2xl font-Gilro italic"
           >
-            {/* <span
-            className="absolute inset-0 bg-no-repeat bg-center bg-cover"
-            style={{ buttonBackground }}
-          /> */}
-            <span className="relative">HandBook</span>
+            {/* SVG Background */}
+            <span className="absolute inset-0 -z-[-1]">
+              <img
+                src={buttonBackground}
+                alt="ButtonBackground"
+                className="w-full h-full object-fill"
+              />
+            </span>
+
+            {/* Button Text */}
+            <span className="relative z-10">Handbook</span>
           </a>
         </div>
       </section>
@@ -102,15 +119,13 @@ const HeroSection = () => {
 };
 const RightCurved = () => {
   return (
-    <div className="absolute w-[457px] h-[700px] left-360 ">
+    <div className="absolute w-full max-w-[457px] h-auto top-0 left-0 sm:left-[20%] md:left-[30%] lg:left-[75%]">
       <svg
-        width="100%"
-        height="100%"
+        className="w-full h-auto"
         viewBox="0 0 457 818"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         preserveAspectRatio="none"
-        className="absolute top-0 left-0 m-o"
       >
         <defs>
           <linearGradient
@@ -136,4 +151,40 @@ const RightCurved = () => {
     </div>
   );
 };
+
+const LeftCurved = () => {
+  return (
+    <div className="absolute w-full max-w-[457px] h-auto top-0 right-0 sm:right-[20%] md:right-[30%] lg:right-[74%] scale-x-[-1]">
+      <svg
+        className="w-full h-auto"
+        viewBox="0 0 457 818"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="none"
+      >
+        <defs>
+          <linearGradient
+            id="paint0_linear_2717_129"
+            x1="271.184"
+            y1="0"
+            x2="515.554"
+            y2="675.009"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#EFC7B0" stopOpacity="0" />
+            <stop offset="0.515" stopColor="#EFC7B0" />
+            <stop offset="1" stopColor="#EFC7B0" stopOpacity="0" />
+          </linearGradient>
+        </defs>
+        <path
+          d="M367.267 0C367.267 0 429.618 176.356 367.267 253C290.623 347.215 156.814 184.874 56.2672 253C-36.8146 328.5 -4.89685 660 96.6852 758.5C262.603 919.384 540.685 697.5 540.685 697.5"
+          stroke="url(#paint0_linear_2717_129)"
+          strokeWidth="1.5"
+          vectorEffect="non-scaling-stroke"
+        />
+      </svg>
+    </div>
+  );
+};
+
 export default HeroSection;
